66746337e219716653eeb453232eac9a3a952438
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-03-29 18:03:53 -0300

    correção na function

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express()
 
 const port = 8080;
  
-app.get('/', function (req, res) {
-    const firstName = faker.name.firstName();
-    const lastName= faker.name.lastName();
-    const gender =faker.name.gender();
-    const birth = faker.date.past(50,2005).toLocaleDateString();
-    const email = faker.internet.email(`${firstName} ${lastName}`);
-    const password = faker.internet.password();
+app.get('/',  (req, res)=> {
+     firstName = faker.name.firstName();
+     lastName= faker.name.lastName();
+     gender =faker.name.gender();
+     birth = faker.date.past(50,2005).toLocaleDateString();
+     email = faker.internet.email(`${firstName} ${lastName}`);
+     password = faker.internet.password();
     
     return res.send(
     person = {
